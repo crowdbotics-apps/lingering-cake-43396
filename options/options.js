5cb81d4de389bda11c5d595de7c733e81bb5c1af
Crowdbotics <team@crowdbotics.com>
2023-09-15 03:30:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "lingering-cake-43396",
+  url: "https://lingering-cake-43396.botics.co",
+  api: "https://lingering-cake-43396.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
